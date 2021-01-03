aba957d93bd5f1bff785e182d3abd08f758e5596
Dan Smatera <dana.smatera@gmail.com>
2021-01-03 20:51:32 +0100

    Registrace & Composer
    Přidána registrace. Oprava labelů + přidán Composer pro sestavení app.

---
+++ app/presenters/RegistracePresenter.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class RegistracePresenter extends Nette\Application\UI\Presenter
+{
+	/** @var Nette\Database\Context */
+	private $database;
+
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+    
+	public function renderDefault(): void
+	{
+	}
+
+	protected function createComponentNewForm(): Form
+	{
+		$form = new Form;
+		$form->addText('username', 'Přezdívka:')->setRequired('Please enter your username.');
+		$form->addPassword('password', 'Heslo:')->setRequired('Please enter your password.');
+		$form->addSubmit('send', 'Registrovat');
+		$form->onSuccess[] = [$this, 'formSucceeded'];
+		return $form;
+	}
+
+	public function formSucceeded(Form $form, $data): void
+	{
+        $row = $this->database->table('uzivatele')->where('prezdivka', $data->username)->fetch();
+        if ($row) {
+			$this->flashMessage('Tento uživatel již existuje!');
+            $this->redirect('Registrace:');
+		}
+        else {
+            $user['role_id'] = 2;
+            $user['prezdivka'] = $data->username;
+            $user['heslo'] = md5($data->password);
+            $this->database->table('uzivatele')->insert($user);
+            $this->flashMessage('Byli jste zaregistrováni. Můžete se přihlásit.');
+            $this->redirect('Sign:in:');
+        }
+	}    
+}
--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -16,13 +16,13 @@ final class SignPresenter extends Nette\Application\UI\Presenter
 	protected function createComponentSignInForm(): Form
 	{
 		$form = new Form;
-		$form->addText('username', 'Username:')
+		$form->addText('username', 'Nick:')
 			->setRequired('Please enter your username.');
 
 		$form->addPassword('password', 'Password:')
 			->setRequired('Please enter your password.');
 
-		$form->addSubmit('send', 'Sign in');
+		$form->addSubmit('send', 'Přihlásit se');
 
 		// call method signInFormSucceeded() on success
 		$form->onSuccess[] = [$this, 'signInFormSucceeded'];
